8ce8d00482c0f752e5add8a1082f38c4d639663f
Nico Kurmann <kurmannn@ethz.ch>
2015-10-27 11:26:24 +0100

    reducer working under linux

--- code/reducer.py
+++ code/reducer.py
@@ -7,13 +7,15 @@ import numpy as np
 
 if __name__ == "__main__":
     w_count =0
-    w = np.zeros(400)
+    w = np.zeros(4)
     #average all incoming w
     for line in sys.stdin:
         line = line.strip()
         w_count = w_count +1
-        w += np.array(map(float, line.split(' ')))
+        w = w+ np.array(map(float, line.split(' ')))
     w = w / w_count
+				
+    print(w)
 				
 	
 							
